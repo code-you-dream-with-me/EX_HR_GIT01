79f3b152a916f025b0ac3a9cb694c13cd18ad8f3
jamesol <jamesol@paran.com>
2021-04-16 17:36:06 +0900

    03번째 master commit

--- EX_HR_GIT01/src/com/sist/ehr/Hello.java
+++ EX_HR_GIT01/src/com/sist/ehr/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world.");
 		System.out.println("01. Hotfix Hello, world.");
+		System.out.println("02. master Hello, world.");
+		
 	}
 
 }
