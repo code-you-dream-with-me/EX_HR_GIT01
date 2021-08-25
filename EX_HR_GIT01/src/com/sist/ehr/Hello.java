753a89f4553fc0bff8f89db33a5b559be41a2cd5
hykim-king <hykim22@gmail.com>
2021-08-25 21:18:24 +0900

    init

---
+++ EX_HR_GIT01/src/com/sist/ehr/Hello.java
@@ -0,0 +1,12 @@
+package com.sist.ehr;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world.");
+		System.out.println("01. Hotfix Hello, world.");
+		System.out.println("02. master Hello, world.");
+		System.out.println("02. Hotfix Hello, world.");	
+	}
+
+}
